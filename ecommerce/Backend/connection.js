d0fc4dbe49cea56a51f63b6e0bc14bf9606d98b2
shahanasherin107 <shahanapm12@gmail.com>
2025-09-08 21:19:54 +0530

    Delete ecommerce directory

--- ecommerce/Backend/connection.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require('mongoose')
-mongoose.connect("mongodb+srv://shahanapm12:shahanasheri@cluster0.yswkjkb.mongodb.net/user?retryWrites=true&w=majority&appName=Cluster0")
-.then(() => console.log('Connected!'))
-.catch((err)=>console.log(err))
